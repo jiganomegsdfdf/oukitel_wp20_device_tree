d5e8ad86d8eebc7d425b420a592d0d0af8b3a272
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2023-07-06 20:48:41 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_WP20.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_WP20-user \
+    omni_WP20-userdebug \
+    omni_WP20-eng
---
+++ omni_WP20.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from WP20 device
+$(call inherit-product, device/wheatek/WP20/device.mk)
+
+PRODUCT_DEVICE := WP20
+PRODUCT_NAME := omni_WP20
+PRODUCT_BRAND := OUKITEL
+PRODUCT_MODEL := WP20
+PRODUCT_MANUFACTURER := wheatek
+
+PRODUCT_GMS_CLIENTID_BASE := android-wheatek
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_te656_oq_s98_61_s0_eea-user 12 SP1A.210812.016 1654769482 release-keys"
+
+BUILD_FINGERPRINT := OUKITEL/WP20_EEA/WP20:12/SP1A.210812.016/1654769482:user/release-keys

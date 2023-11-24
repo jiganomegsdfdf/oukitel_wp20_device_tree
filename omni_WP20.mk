7094a61584c72354dd42310eb10d2143c601bf6a
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2023-11-24 19:34:46 +0300

    Add files via upload

--- omni_WP20.mk
+++ omni_WP20.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-#$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from WP20 device
 $(call inherit-product, device/wheatek/WP20/device.mk)
@@ -24,6 +24,6 @@ PRODUCT_MANUFACTURER := wheatek
 PRODUCT_GMS_CLIENTID_BASE := android-wheatek
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_te656_oq_s98_61_s0_eea-user 12 SP1A.210812.016 1654769482 release-keys"
+    PRIVATE_BUILD_DESC="full_te656_oq_s98_61_s0_eea-user 12 SP1A.210812.016 1678970077 release-keys"
 
-BUILD_FINGERPRINT := OUKITEL/WP20_EEA/WP20:12/SP1A.210812.016/1654769482:user/release-keys
+BUILD_FINGERPRINT := OUKITEL/WP20_EEA/WP20:12/SP1A.210812.016/1678970077:user/release-keys
